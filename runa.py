1ba662beef368a2fe55a008c8b29090815972d23
TPHsMP <o.g.kcity@gmail.com>
2022-10-03 13:52:59 +0600

    добавлен новый атрибут класса Runa - runa_image - для вывода изображения руны. Переписан класс RunesLayout - созданы ращные методы для обработки 1 и нескольких рун

--- runa.py
+++ runa.py
@@ -1,4 +1,6 @@
 import random
+from enum import Enum
+from typing import List
 
 
 class Runa:
@@ -6,7 +8,7 @@ class Runa:
     # letter и second_letter - соответсвующая буква английского алфавита
     # description и description_inverted - толкование руны в прямом и перевернутом положении
     def __init__(self, name, description, descriptionInverted, position, ascii, letter, second_letter, image,
-                 image_inversed):
+                 image_inversed, send_image):
         self.name = name
         self.description = description
         self.descriptionInverted = descriptionInverted
@@ -19,6 +21,7 @@ class Runa:
         self.second_letter = second_letter
         self.image = image
         self.image_inversed = image_inversed
+        self.send_image = send_image
 
     def __str__(self):
         if self.position == 0:
@@ -28,16 +31,21 @@ class Runa:
 
 
 class RunesLayout:
+    # выборка для одной руны
     @classmethod
-    async def get_rune(cls, runa, runes_list, message=None):
-        from aiogram_main import bot
-        from lists import runes
+    def get_one_rune(cls, runes):
+        runa = random.choice(runes)
+        runa.send_image = runa.image_inversed if runa.position == 1 else runa.image
+        return runa
+
+    # выборка для трех и более рун
+    @classmethod
+    def get_list_runes(cls, runes, count=3):
+        runes_for_send: List[Runa] = []
         runes_list = runes.copy()
-        with open(runa.image_inversed if runa.position == 1 else runa.image, 'rb') as runa_image:
-            await bot.send_photo(message, photo=runa_image, disable_notification=True)
-        # if runa.position == 1:
-        #     with open(runa.image_inversed, 'rb') as runa_image:
-        #         await bot.send_photo(message, photo=runa_image, disable_notification=True)
-        runa1 = str(runa)
-        runes_list.remove(runa)
-        await bot.send_message(message, runa1)
+        for i in range(count):
+            runa = random.choice(runes_list)
+            runa.send_image = runa.image_inversed if runa.position == 1 else runa.image
+            runes_for_send.append(runa)
+            runes_list.remove(runa)
+        return runes_for_send
